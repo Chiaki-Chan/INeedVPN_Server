e1423b15e4f9e2594319c86d90149abcf879b072
Chiaki <root@whatisit.cn>
2017-08-22 13:32:41 +0800

    Add files via upload

---
+++ shadowsocks/Config.py
@@ -0,0 +1,12 @@
+#Config
+MYSQL_HOST = 'www.ineedvpn.net'
+MYSQL_PORT = 3306
+MYSQL_USER = 'root'
+MYSQL_PASS = 'Ch1@kiLeft'
+MYSQL_DB = 'INeedVPN'
+
+MANAGE_PASS = 'ss233333333'
+#if you want manage in other server you should set this value to global ip
+MANAGE_BIND_IP = '127.0.0.1'
+#make sure this port is idle
+MANAGE_PORT = 23333
